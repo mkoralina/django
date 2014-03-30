afd89bc5cb148dae56bd3517bfade15f08a526ea
mkoralina <mo.konopka@gmail.com>
2014-03-30 22:20:57 +0200

    Trzecia czesc, 404

--- a/views.py
+++ a/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,7 +11,7 @@ def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
 
     context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'a/index.html', context)
 
     #template = loader.get_template('a/index.html')
     #context = RequestContext(request, {
@@ -21,8 +22,14 @@ def index(request):
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the poll index.")    
 
+
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'a/detail.html', {'poll': poll})
+
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
